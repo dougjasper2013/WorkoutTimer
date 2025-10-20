34fde980f501f51026808949adca4a031115c878
Douglas Jasper <doug.jasper@trios.com>
2025-10-20 08:17:32 -0400

    Initial Commit

---
+++ WorkoutTimer Watch App/WorkoutTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutTimerApp.swift
+//  WorkoutTimer Watch App
+//
+//  Created by Douglas Jasper on 2025-10-20.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutTimer_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WorkoutTimer Watch AppTests/WorkoutTimer_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutTimer_Watch_AppTests.swift
+//  WorkoutTimer Watch AppTests
+//
+//  Created by Douglas Jasper on 2025-10-20.
+//
+
+import Testing
+@testable import WorkoutTimer_Watch_App
+
+struct WorkoutTimer_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
